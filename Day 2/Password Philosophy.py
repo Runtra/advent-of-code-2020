d08de4d992b49fbf94050f35e51ec93843cde993
Runtra <50464463+Runtra@users.noreply.github.com>
2020-12-02 03:35:23 -0300

    Day 2, part 2

--- Day 2/Password Philosophy.py
+++ Day 2/Password Philosophy.py
@@ -2,23 +2,44 @@ with open("input.txt", "r") as f:
 	passwords = f.readlines()
 
 
-def checkPassword(line):
-		splitted = line.split()
+def oldCheckPassword(line):
 
-		policyRange = splitted[0].split("-") 
-		minRange = int(policyRange[0])
-		maxRange = int(policyRange[1])
+	splitted = line.split()
+
+	policyRange = splitted[0].split("-") 
+	minRange = int(policyRange[0])
+	maxRange = int(policyRange[1])
 		
-		policyLetter = splitted[1].replace(":","")
-		password = splitted[2]
+	policyLetter = splitted[1].replace(":","")
+	password = splitted[2]
 
-		if password.count(policyLetter) in range(minRange, maxRange+1):
-			return True
-		else:
-			return False
+	if password.count(policyLetter) in range(minRange, maxRange+1):
+		return True
+	else:
+		return False
+
+def newCheckPassword(line):
+
+	splitted = line.split()
+
+	toCheckValues = splitted[0].split("-") 
+	value1 = int(toCheckValues[0])-1
+	value2 = int(toCheckValues[1])-1
+		
+	policyLetter = splitted[1].replace(":","")
+	password = splitted[2]
+
+	if bool(password[value1] == policyLetter) != bool(password[value2] == policyLetter):
+		return True
+	else:
+		return False
+
+
+
 
 correctPasswords = 0
+
 for lines in passwords:
-	if checkPassword(lines):
+	if newCheckPassword(lines):
 		correctPasswords += 1
 print(correctPasswords)

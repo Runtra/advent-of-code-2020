58bf0d79bb2707543685f328df0b0d2536f3b1d2
Runtra <50464463+Runtra@users.noreply.github.com>
2020-12-06 08:53:26 -0300

    Finally, Day 4
    I tried my best but at the end some little things werent working, so i looked the megathread

--- Day 4/Passport Processing.py
+++ Day 4/Passport Processing.py
@@ -1,93 +1,96 @@
 with open("input.txt", "r") as f:
-	passports = f.readlines()
-	passports = list(map(lambda s: s.strip(), passports)) # Removes the \n at the end of every line
+	passportsInput = f.readlines()
+	passportsInput = list(map(lambda s: s.strip(), passportsInput)) # Removes the \n at the end of every line
 
 
 splitIndexes = []
-
-
-for lines in passports:
+# Removes the empty line between each group and saves the index
+for lines in passportsInput:
 	if lines == '':
-		indexOfSplit = passports.index(lines)
+		indexOfSplit = passportsInput.index(lines)
 		splitIndexes.append(indexOfSplit)
-		passports.pop(indexOfSplit)
-
-splittedPassports = [passports[i : j] for i, j in zip([0] + 
+		passportsInput.pop(indexOfSplit)
+# Divides the groups into lists inside the list, each group marked by the saved indexes
+splittedPassports = [passportsInput[i : j] for i, j in zip([0] + 
           splitIndexes, splitIndexes + [None])] 
-#print(splittedPassports)
 
 validPassports = 0
-isValid = False
-possibleHairColors = ['amb', 'blu', 'brn', 'gry', 'grn', 'hzl', 'oth']
 for lines in splittedPassports:
 
 	for item in lines:
-		if item.find(' ') != -1:
-			itemIndx = lines.index(item)
-			
-			item = item.split()
 
+		if ' ' in item:
+
+			#lines.remove(item)
+			#item = item.split()
+			#lines.extend(item)
+
+			itemIndx = lines.index(item)
+			item = item.split()
 			lines = lines[:itemIndx] + item + lines [itemIndx + 1:]
 	
 	for item in lines:
 		itemIndx = lines.index(item)
 		item = item.split(':')
 		lines = lines[:itemIndx] + item + lines [itemIndx + 1:]
-	#print(lines)
+
 	
 	passportDict = {}
-	# De aca para abajo chequeamos la validez del pasaporte
 	for i in range(0, len(lines), 2):
 		passportDict.update({lines[i]:lines[i+1]})
-	print(passportDict)
 
-	checkParametersValidity = False
 
+	flag = False
 	if len(passportDict) == 8:
-		checkParametersValidity = True
-	elif len(passportDict) == 7:
-		cidFound = False
-		for item in lines:
-			if 'cid' in item:
-				cidFound = True
-				break
-		if not cidFound:
-			checkParametersValidity = True
-
-	if checkParametersValidity: # Ya se que esta no es la manera correcta de hacerlo pero no me rompas la pija son las 6 de la mañana y hace tanto tiempo que no duermo
-		isValid = True
-		if 1920 <= int(passportDict['byr']) <= 2002:
-			print(passportDict['byr'])
-		else:
-			isValid = False
-			print('byr: False -> ' + passportDict['byr'])
-		if 2010 <= int(passportDict['iyr']) <= 2020:
-			print(passportDict['iyr'])
-		else:
-			isValid = False
-			print('iyr: False -> ' + passportDict['iyr'])
-		if 2020 <= int(passportDict['eyr']) <= 2030:
-			print(passportDict['eyr'])
-		else:
-			isValid = False
-			print('eyr: False -> ' + passportDict['eyr'])
-		if passportDict['hcl'] in possibleHairColors:
-			print(passportDict['hcl'])
-		else:
-			isValid = False
-			print('hcl: False -> ' + passportDict['hcl'])
-		if passportDict['pid'].isdigit() and len(passportDict['pid']) == 9:
-			print(passportDict['pid'])
-		else:
-			isValid = False
-			print('pid: False')
-		# encima no anfa esto la concha de la lora
+		flag = True
+	elif len(passportDict) == 7 and 'cid' not in passportDict:
+		flag = True
 
 
-	if isValid:
+	if flag:
+		if not (1920 <= int(passportDict['byr']) <= 2002):
+			flag = False
+		if not (2010 <= int(passportDict['iyr']) <= 2020):
+			flag = False
+		if not (2020 <= int(passportDict['eyr']) <= 2030):
+			flag = False
+		# This conditional is taken from the solution megathread because i couldn't make it work and i've already looked that answer so my logic was "corrupt" anyways
+		if not (((passportDict['hgt'][-2:] == 'cm') and (150 <= int(passportDict['hgt'][:-2]) <= 193)) or ((passportDict['hgt'][-2:] == 'in') and (59 <= int(passportDict['hgt'][:-2]) <= 76))):
+			flag = False
+		if not (passportDict['hcl'][0] == '#' and int(passportDict['hcl'][1:],16) > 0):
+			flag = False
+		if passportDict['ecl'] not in ['amb', 'blu', 'brn', 'gry', 'grn', 'hzl', 'oth']:
+			flag = False
+		if not (passportDict['pid'].isdigit() and len(passportDict['pid']) == 9):
+			flag = False
+
+	if flag:
 		validPassports += 1
-		
+
 	
+
 print(validPassports)
-				
-# por favor no me pidas que entienda este codigo en media hora porque no tengo ni la mas puta idea
+
+
+'''
+This is one of the attemps i did after day 4, if you want to look...
+#splittedPassports = [['byr:1935 eyr:2025 iyr:2018 hgt:65in pid:396444938 cid:293', 'ecl:grn', 'hcl:#efcc98'], ['byr:1926 pid:954310029 cid:64', 'hcl:#fffffd', 'ecl:grn', 'iyr:2012', 'hgt:150cm', 'eyr:2024']]
+valid = 0
+for passport in splittedPassports:
+	#print(passport)
+	for line in passport:
+		#print(line)
+		if ' ' in line:
+			passport.remove(line)
+			line = line.split()
+			passport.extend(line)
+
+		#print(line)
+	print(passport)
+
+	if len(passport) == 8:
+		valid += 1
+	elif len(passport) ==  and 'cid' not in passport:
+		valid += 1
+print(valid)
+'''
